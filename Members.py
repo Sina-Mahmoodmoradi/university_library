292dfa0774de9b6715aaa4a88d82ac2cb254628c
Sina Mahmoodmoradi <sina.mahmudmoradi@gmail.com>
2022-06-02 14:00:25 +0430

    add init function to class

--- Members.py
+++ Members.py
@@ -1,2 +1,10 @@
+from mimetypes import init
+
+
 class Member:
-    pass
+    def __init__(self, first_name, last_name, student_number, student_id, lent_books=0) -> None:
+        self.first_name = first_name
+        self.last_name = last_name
+        self.student_number = student_number
+        self.student_id = student_id
+        self.lent_books = lent_books

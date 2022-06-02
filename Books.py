9a31a2091ef7bf4dd9a69212bbe1bf749f0145d6
Sina Mahmoodmoradi <sina.mahmudmoradi@gmail.com>
2022-06-02 14:01:00 +0430

    add init function to class

--- Books.py
+++ Books.py
@@ -1,2 +1,11 @@
+from turtle import title
+
+
 class Book:
-    pass
+    def __init__(self, title, author, year_of_publication, isbn, number_of_books, available=0) -> None:
+        self.title = title
+        self.author = author
+        self.year_of_publication = year_of_publication
+        self.isbn = isbn
+        self.number_of_books = number_of_books
+        self.available = available

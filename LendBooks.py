f38adb2e77889725fa5a39443c79e1c4dc22740e
Sina Mahmoodmoradi <sina.mahmudmoradi@gmail.com>
2022-06-05 15:27:04 +0430

    add method lend

--- LendBooks.py
+++ LendBooks.py
@@ -1,5 +1,32 @@
+from datetime import date, timedelta
+from Books import Book
+from Members import Member
+from FileHandler import FileHandler as fl
+
+
 class LendBook:
-    def __init__(self, isbn, student_number, date) -> None:
+    def __init__(self, isbn, student_number, date_lend=date.today()) -> None:
         self.isbn = isbn
         self.student_number = student_number
-        self.date = date
+        self.date_lend = date_lend
+        self.date_return = date_lend + timedelta(days=7)
+
+    def lend(self):
+        book = Book.get_book_by_isbn(self.isbn)
+        if not book:
+            print('ISBN does not match!')
+            return
+        if book.available == 0:
+            print('book is not available!')
+            return
+
+        member = Member.get_member_by_student_number(self.student_number)
+        if not member:
+            print('Student number does not match!')
+            return
+        if member.lent_books == 3:
+            print('student has already have 3 books!')
+            return
+
+        fl.lend_book(self)
+        print('done!')

173c24a63471c0d88a899950a7af3e540b536932
Sina Mahmoodmoradi <sina.mahmudmoradi@gmail.com>
2022-06-05 13:13:08 +0430

    add methods of class member

--- Members.py
+++ Members.py
@@ -1,3 +1,6 @@
+from FileHandler import FileHandler as fl
+
+
 class Member:
     def __init__(self, first_name, last_name, student_number, student_id, lent_books=0) -> None:
         self.first_name = first_name
@@ -5,3 +8,31 @@ class Member:
         self.student_number = student_number
         self.student_id = student_id
         self.lent_books = lent_books
+
+    def add_member(self):
+        members = fl.get_all_members()
+        members[self.student_number] = self
+        fl.add_members_to_file(members)
+
+    # use for check before adding new member
+    def member_exists(student_number):
+        return student_number in fl.get_all_members()
+
+    def search_member_by_name(name):
+        members = fl.get_all_members().values()
+        results = filter(lambda member: name in member.name, members)
+        return list(results)
+
+    def search_member_by_author(student_id):
+        members = fl.get_all_members().values()
+        for member in members:
+            if member.student_id == student_id:
+                return member
+        return False
+
+    def search_member_by_isbn(student_number):
+        members = fl.get_all_members()
+        if student_number in members:
+            return members[student_number]
+        else:
+            return False
